e2c5711fffa3eef2af276d068a595659f1de6fcb
josseth <jossethale32@hotmail.com>
2022-06-11 18:00:48 -0600

    semana antes del examen IMPLEMENTAR collection

---
+++ PROEHECT/PROEHECT/Views/EmplePageCollection.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PROEHECT.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EmplePageCollection : ContentPage
+    {
+        public EmplePageCollection()
+        {
+            InitializeComponent();
+        }
+        
+        private void ListaEmple_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            ListaEmple.ItemsSource = await App.DBase.ObtenerListaEmple();
+        }
+    }
+}
